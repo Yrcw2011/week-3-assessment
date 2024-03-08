78f45fa3443b2200e14a228fd10cdbe7ceba2c5e
Mitch Marsh <NewCoder2024@gmail.com>
2024-03-08 11:44:37 -0700

    Maybe

---
+++ about.js
@@ -0,0 +1,18 @@
+console.log("hello world");
+
+
+
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+	console.log('form submit');
+	
+}
+
+
+let form = document.querySelector('#contact');
+
+form.addEventListener('submit', handleSubmit);
+
+img.addEventListener('mouseover', Cat)
+alert('Successfully Submitted')
---
+++ profile.js
@@ -0,0 +1,12 @@
+document.getElementById('color');
+alert("My favorite color is navy blue");
+
+document.getElementById('place');
+alert("My favorite place is Home");
+
+document.getElementById('ritual');
+alert("My favorite ritual is fluffing pillows before bed");
+
+addEventListener('click', My-Favorite-Color)
+addEventListener('click', My-Favorite-Place)
+addEventListener('click', My-Favorite-Ritual)
