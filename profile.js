ab865ce7bae1ecd3abe9c26e6db6d21c3f2b8252
Mitch Marsh <NewCoder2024@gmail.com>
2024-03-10 18:35:48 -0600

    last.night

--- profile.js
+++ profile.js
@@ -7,6 +7,18 @@ alert("My favorite place is Home");
 document.getElementById('ritual');
 alert("My favorite ritual is fluffing pillows before bed");
 
+document.getElementById('number');
+alert("My favorite number is 50");
+
+document.getElementById('animal');
+alert("My favorite animal is a cat");
+
+document.getElementById('ritual');
+alert("My favorite hobby is playing golf");
+
 addEventListener('click', My-Favorite-Color)
 addEventListener('click', My-Favorite-Place)
 addEventListener('click', My-Favorite-Ritual)
+addEventListener('click', My-Favorite-number)
+addEventListener('click', My-Favorite-animal)
+addEventListener('click', My-Favorite-hobby)
